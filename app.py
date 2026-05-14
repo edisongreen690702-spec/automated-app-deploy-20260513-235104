628300f337d14c0528e66d051dc70aa5fa53b91a
edisongreen690702-spec <edisongreen690702@gmail.com>
2026-05-14 20:59:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -249,5 +249,5 @@ def dashboard():
 
 # --- 伺服器啟動 ---
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
